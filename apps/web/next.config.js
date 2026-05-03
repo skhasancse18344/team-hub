a7a9b04d52e949debc57ed4f43ee9af5150e24f9
Kamrul Hasan <kamrul.hasan.cse18344@gmail.com>
2026-05-03 14:06:48 +0600

    fix: nex config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone", // ✅ ADD THIS
+
   transpilePackages: ["@repo/ui"],
+
   images: {
     remotePatterns: [
       {
